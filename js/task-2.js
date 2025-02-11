bd9fef560e8a65e439091abb8a9ac8273ba1c132
Metehan Duman <metedum@gmail.com>
2025-02-11 11:02:22 +0300

    first-commit

---
+++ js/task-2.js
@@ -0,0 +1,23 @@
+class Storage {
+    #items;
+    
+    constructor(initialItems) {
+        this.#items = initialItems;
+    }
+    getItems() { 
+        return this.#items;
+    }
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+    removeItem(removingItem) {
+        this.#items = this.#items.filter(item => item !== removingItem);
+    }
+}
+
+const storage = new Storage(["Nanitoid", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoid", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoid", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoid", "Antigravitator", "Droid"]
